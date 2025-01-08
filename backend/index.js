bc75e0356ac112c8098c2bbccfa7821d8866431c
Rafael Zerna <nvzerna@up.edu.ph>
2025-01-09 04:01:44 +0800

    Added dotenv in backend

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 
-// packages installed: express, mongoose, nodemon
+// packages installed: express, mongoose, nodemon, dotenv
 // npm run dev
-console.log('test')
+
+
+
+dotenv.configDotenv({path:'../.env'})
+const MONGO_URI = process.env.MONGO_URI
+
+
+
+console.log(MONGO_URI)
