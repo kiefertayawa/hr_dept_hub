1e12ca9169c7509a2ef102ab16dd6021fd6a9920
rmcaoile <rmcaoile@up.edu.ph>
2025-01-10 21:47:45 +0800

    feat: backend posting of new member to db

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,18 @@ import userRoutes from './routes/userRoutes.js'
 dotenv.configDotenv({path:'../credentials.env'})
 const MONGO_URI = process.env.MONGO_URI
 const PORT = 4000 //process.env.PORT
+
+// Express app
 const app = express()
+
+// Middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
 
 mongoose.connect(MONGO_URI)
     .then(()=>{
