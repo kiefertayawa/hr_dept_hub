70f398218eb399283d9b9f7e30514c29bc820104
Rafael Zerna <nvzerna@up.edu.ph>
2025-01-12 16:14:10 +0800

    feat: integrated login page and family tree page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,21 @@
 import Login from "./Login/Login.jsx"
 import FamilyTree from "./FamilyTree/FamilyTree.jsx"
 import Header from "./Header.jsx"
-import bg from "./assets/bg video black.mp4"; 
-
+import bg from "./assets/bg video black.mp4"
+import { useState } from "react"
 
 function App() {
+  
+  const [login, setLogin] = useState(false)
+
   return(
     <>
       <video className="background-video" autoPlay loop muted>
         <source src={bg} type="video/mp4" />
       </video>
-      <Header />
-      {/* <Login /> */}
-      <FamilyTree />
+      <Header loginClick={setLogin}/>
+      { login && <Login /> }
+      { !login && <FamilyTree />}
     </>
    
     // <div className='App'>
