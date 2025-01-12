4a4aa8fe8643f8ccfbe989ab70b5b5a2a85a5d8e
Rafael Zerna <nvzerna@up.edu.ph>
2025-01-12 11:08:18 +0800

    feat: buttons for switching bloodlines

--- frontend/src/FamilyTree/FamilyTreePage.jsx
+++ frontend/src/FamilyTree/FamilyTreePage.jsx
@@ -5,6 +5,7 @@ import * as d3 from 'd3';
 export default function FamilyTreePage(props) {
     
     const [data, setData] = useState(null)
+    const [index, setIndex] = useState(0)
 
     useEffect(() => {
         d3.json('http://localhost:4000/api/member/getAll')
@@ -13,16 +14,23 @@ export default function FamilyTreePage(props) {
         })
     }, [true]);
     
+    function switchBloodline(direction) {
+        if(data && index+direction >= 0 && index+direction <= data.length)
+        {
+            console.log(index)
+            setIndex(index+direction)
+            console.log(index)
+        }
+    }
+
     return (
         <>
+            <button onClick={() => switchBloodline(-1)}>Left</button>
+            <button onClick={() => switchBloodline(1)}>Right</button>
+            {/* <button onClick={switchBloodline('right')}>Right</button> */}
             {
-                data && <Bloodline data={data[10]} />
+                data && <Bloodline data={data[index]} />
             }
-            {/* {
-                data && data.map(bloodline => {
-                   return <Bloodline data={bloodline}/>
-                })
-            } */}
         </>
     )
 }
