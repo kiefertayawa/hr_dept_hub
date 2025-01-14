1945ea2da099f2556966303d7eb5c284b014eade
scpepito <scpepito@up.edu.ph>
2025-01-14 20:27:35 +0800

    chore: added separate css file for InfoField.jsx

--- frontend/src/FamilyTree/InfoField.jsx
+++ frontend/src/FamilyTree/InfoField.jsx
@@ -1,47 +1,15 @@
+import React from "react";
+import "./InfoField.css"; 
 
-
-export default function InfoField({label, data}){
-    
-    const containerStyle = {
-        width: "100%",
-        height: "15%",
-        marginTop: "10%",
-        color: "black",
-    }
-
-    const labelStyle = {
-        marginBottom: "5px",
-        color: "#003366",
-        fontWeight: "bold",
-        fontSize: "150%",
-        marginLeft: "10px",
-    }
-
-    const infoStyle = {
-        maxWidth: "90%",
-        padding: "0.01%",
-        borderRadius: "40px",
-        marginLeft: "5%",
-        marginRight: "5%",
-        border: "none",
-        color: "white",
-        backgroundColor: "#003366",
-        fontSize: "150%",
-        opacity: "0.85",
-    }
-    
-    return(
-        <>
-            {/* {console.log(type)} */}
-            {console.log(data)}
-            <div style={containerStyle}>
-                <div style={labelStyle}>
-                    {label}
-                </div>
-                <div style={infoStyle}>
-                    <p>{data}</p>
-                </div>
+export default function InfoField({ label, data }) {
+    return (
+        <div className="container">
+            <div className="label">
+                {label}
             </div>
-        </>
-    )
+            <div className="info">
+                <p>{data}</p>
+            </div>
+        </div>
+    );
 }
