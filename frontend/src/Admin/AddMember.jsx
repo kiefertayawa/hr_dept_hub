78161458982c8baca63667abf2cc454ac60bf7e5
ian <icarragona@up.edu.ph>
2025-01-15 21:19:58 +0800

    created add member view

---
+++ frontend/src/Admin/AddMember.jsx
@@ -0,0 +1,37 @@
+import defaultPic from "../assets/default pic.jpeg"
+import uploadIcon from "../assets/upload icon.png"
+
+export default function addMember(){
+ return (
+         <>
+         <div className="form">
+            <form className="field">
+                <div className="upload-photo">
+                    <img src={defaultPic} alt="default pic"/>
+                    <img src={uploadIcon} alt="upload pic" />
+                    <button>UPLOAD PIC</button>
+                </div>
+
+                <div className="details">
+                    <h2>Mentor</h2>
+                    <div className="mentor-name"></div>
+                    
+                    <label htmlFor="name">Name</label>
+                    <input type="text" id="name" name="name"/>
+                    
+                    <label htmlFor="orgbatch">YSES BATCH</label>
+                    <input type="text" id="orgbatch" name="orgbatch"/>
+
+                    <label htmlFor="collegebatch">COLLEGE BATCH</label>
+                    <input type="text" id="collegebatch" name="collegebatch"/>
+                    
+                    <label htmlFor="level">LEVEL</label>
+                    <input type="number" id="level" name="level"/>
+
+                    <button type="submit" className="save-btn">SAVE</button>
+                </div>
+            </form>
+        </div>
+        </>
+     )   
+}
