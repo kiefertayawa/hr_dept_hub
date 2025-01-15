3f884b6d50f37d3fdfe9f1a56a257637777d9307
kiefertayawa <kltayawa@up.edu.ph>
2025-01-16 03:53:55 +0800

    ui: added upload pic and save button to admin view

--- frontend/src/Admin/AdminMemberInfo.jsx
+++ frontend/src/Admin/AdminMemberInfo.jsx
@@ -3,6 +3,7 @@ import memberImg from "../assets/default pic.jpeg";
 import "./AdminMemberInfo.css";
 import addButton from "../assets/add icon.png"
 import removeButton from "../assets/remove icon.png"
+import uploadIcon from "../assets/upload icon.png"
 import AddMember from "./AddMember"
 import { useState } from "react";
 
@@ -30,12 +31,17 @@ export default function MemberInfo({exit, name, ysesBatch, collegeBatch, mentor}
                     </button>
                     <div className="image-container">
                         <img src={memberImg} className="member-image" />
+                        <div className="upload-section">
+                            <img src={uploadIcon} alt="Upload Icon" className="upload-icon" />
+                        </div>
+                        <button className="upload-pic-button">UPLOAD PIC</button>
                     </div>
                     <div className="fields-container">
                         <InfoField label={"NAME"} data={name} labelClass="custom-label" />
                         <InfoField label={"YSES BATCH"} data={ysesBatch} labelClass="custom-label" />
                         <InfoField label={"COLLEGE BATCH"} data={collegeBatch} labelClass="custom-label" />
                         {mentor && <InfoField label={"MENTOR"} data={mentor} labelClass="custom-label" />} {/* Don't render if node is a charter member */}
+                        <button className="save-button">SAVE</button>
                     </div>
                 </div>
             </div>
